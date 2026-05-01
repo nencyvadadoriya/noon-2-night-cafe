d0cb7bedcfe2017dcb0bd2b5dad56b5eb46687d2
Nency Vadadoriya <vadadoriyanency8@gmail.com>
2026-05-01 12:15:17 +0530

    add on not found page

--- cafeProject/src/App.tsx
+++ cafeProject/src/App.tsx
@@ -1,14 +1,27 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, useLocation } from "react-router-dom";
+import { useEffect } from "react";
 import Home from "./Home";
 import MenuPage from "./MenuPage";
 import AboutPage from "./AboutPage";
 import TestimonialsPage from "./TestimonialsPage";
 import ContactPage from "./ContactPage";
 import CoffeeKnowledgePage from "./CoffeeKnowledgePage";
+import NotFoundPage from "./NotFoundPage";
+
+function ScrollToTop() {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+
+  return null;
+}
 
 function App() {
   return (
     <Router>
+      <ScrollToTop />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<MenuPage />} />
@@ -16,6 +29,7 @@ function App() {
         <Route path="/testimonials" element={<TestimonialsPage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/coffee-knowledge" element={<CoffeeKnowledgePage />} />
+        <Route path="*" element={<NotFoundPage />} />
       </Routes>
     </Router>
   );
